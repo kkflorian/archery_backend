040e34f6cf93f01ced8a0528d95150f0a9756836
Marcel Pürstinger <masi.puersti@gmail.com>
2020-09-28 15:08:27 +0200

    config 1nice

--- src/main/java/property/abolish/archery/Main.java
+++ src/main/java/property/abolish/archery/Main.java
@@ -1,9 +1,22 @@
 package property.abolish.archery;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        ConnectionConfig load;
 
-        
+        try {
+            load = ConnectionConfig.load();
+        } catch (IOException e) {
+            System.out.println("Config konnte nicht geladen werden!");
+            e.printStackTrace();
+            System.exit(1);
+            return;
+        }
+
+        System.out.println(load.dbIp);
+
 
     }
 }
