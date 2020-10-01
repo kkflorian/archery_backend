d1c5bd7ab8c8209560b6354e244aa71b87194827
Marcel Pürstinger <masi.puersti@gmail.com>
2020-10-01 08:41:36 +0200

    add http models RegisterRequest and LoginRequest

---
+++ src/main/java/property/abolish/archery/http/model/LoginRequest.java
@@ -0,0 +1,6 @@
+package property.abolish.archery.http.model;
+
+public class LoginRequest {
+    String username;
+    String password;
+}
---
+++ src/main/java/property/abolish/archery/http/model/RegisterRequest.java
@@ -0,0 +1,8 @@
+package property.abolish.archery.http.model;
+
+public class RegisterRequest {
+    public String username;
+    public String firstName;
+    public String lastName;
+    public String password;
+}
