4f8e58a5adeda22a6fa79f311f3f7299f8e5fc56
Marcel Pürstinger <masi.puersti@gmail.com>
2020-11-02 11:32:14 +0100

    Issue 8: return error when event not found or not finished

--- src/main/java/property/abolish/archery/http/controller/StatsController.java
+++ src/main/java/property/abolish/archery/http/controller/StatsController.java
@@ -3,8 +3,11 @@ package property.abolish.archery.http.controller;
 import io.javalin.http.Context;
 import org.jdbi.v3.core.Handle;
 import property.abolish.archery.Archery;
+import property.abolish.archery.db.model.Event;
 import property.abolish.archery.db.model.User;
+import property.abolish.archery.db.query.EventQuery;
 import property.abolish.archery.db.query.ShotQuery;
+import property.abolish.archery.http.model.responses.ErrorResponse;
 import property.abolish.archery.http.model.responses.EventStatsResponse;
 import property.abolish.archery.http.model.responses.OverallStatsGraphResponse;
 
@@ -13,6 +16,19 @@ public class StatsController {
         int eventId = ctx.pathParam("eventId", Integer.class).get();
 
         try (Handle dbConnection = Archery.getConnection()) {
+
+            Event event = dbConnection.attach(EventQuery.class).getEventByEventId(eventId);
+
+            if (event == null) {
+                ctx.status(404).json(new ErrorResponse("EVENT_NOT_FOUND", "The event does not exist"));
+                return;
+            }
+
+            if (event.getTimestampEnd() == null) {
+                ctx.status(409).json(new ErrorResponse("EVENT_NOT_FINISHED", "The event is still ongoing"));
+                return;
+            }
+
             ctx.json(new EventStatsResponse(dbConnection.attach(ShotQuery.class).getEventStats(eventId)));
         }
     }
